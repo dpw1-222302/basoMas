ed2d5eb3a001e045464016f1690da37fff7c00ac
Azelia Puspa Diah Narendri <azeliapuspadiahnarendri@gmail.com>
2023-07-25 14:47:00 +0700

    add modal login & register in index.php

--- index.php
+++ index.php
@@ -6,39 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baso Mas</title>
     <link rel="stylesheet" href="assets/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
             <img class="logo" src="assets/img/logo-basoMas.png" alt="">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link active text-dark" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="restaurant.html">Restaurant</a>
+                    <a class="nav-link" href="restaurant.html">Restaurant</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="#" data-toggle="modal" data-target="#exampleModal">Fan Reviews</a>
+                    <a class="nav-link" href="#">Fan Reviews</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="#" data-toggle="modal" data-target="#exampleModal">Masuk</a>
+                    <a class="nav-link" href="#loginModal" data-toggle="modal">Masuk</a>
                 </li>
-            </ul>
 
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-light my-1 my-sm-0" type="submit">Search</button>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Masukkan kata kunci" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
             </form>
         </div>
     </nav>
@@ -71,14 +74,20 @@
                 // menggunakan query sql agar menampilkan data produk dan join kedalam tabel user agar mendapatkan siapa pemilik produk
                 $query = "SELECT * FROM restaurant ";
                 $datas = $conn->query($query);
-                foreach ($datas as $data) :
-                ?>
+                foreach ($datas as $data):
+                    ?>
                     <div class=" container card">
                         <img class="card-img-top" src="..." alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title"><?= $data['nama_resto'] ?></h5>
-                            <h6 class="card text"><?= $data['harga'] ?></h6>
-                            <p class="card-text"><?= $data['lokasi'] ?></p>
+                            <h5 class="card-title">
+                                <?= $data['nama_resto'] ?>
+                            </h5>
+                            <h6 class="card text">
+                                <?= $data['harga'] ?>
+                            </h6>
+                            <p class="card-text">
+                                <?= $data['lokasi'] ?>
+                            </p>
                         </div>
                         <div class="card-footer">
                             <small class="text-muted">Last updated 3 mins ago</small>
@@ -214,45 +223,93 @@
     <!-- Footer -->
 
 
-    <!-- Add the Bootstrap CSS and JavaScript files as mentioned above
+    <!-- Modal Login -->
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                    <h5 class="modal-title font-weight-bold" id="loginModalLabel">MASUK</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="proses_login.php" method="POST">
+                <form action="index.php" method="POST">
                     <div class="modal-body">
                         <form action="proses_login.php" method="POST">
-                            <div class="text-center">
+                            <div class="modal-body">
                                 <label for="email">Email</label>
-                                <input required type="email" class="form-control" id="email" placeholder="Masukan email" name="email">
+                                <input required type="email" class="form-control" id="email" placeholder="Masukan email"
+                                    name="email">
                             </div>
-                            <div class="text-center">
+                            <div class="modal-body">
                                 <label for="password">Password</label>
-                                <input required type="password" class="form-control" id="password" placeholder="Masukkan password" name="password">
+                                <input required type="password" class="form-control" id="password"
+                                    placeholder="Masukkan password" name="password">
                             </div>
-                            <div class="text-center">
-                                <a href="register.php">Belum punya akun? Daftar di sini</a>
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary btn-block">Masuk</button>
-                                </div>
-                            </div>
+                            <div class="modal-body mb-3">Belum punya akun? <a class="regist" href="#registModal"
+                                    data-dismiss="modal" data-toggle="modal">Daftar</a></div>
                         </form>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                            <button type="button" class="btn btn-primary">Masuk</button>
+                        </div>
                     </div>
-
                 </form>
             </div>
         </div>
     </div>
- -->
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <!-- Modal Regist -->
+
+    <div class="modal" id="registModal" tabindex="-1" aria-labelledby="registModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title font-weight-bold" id="registModalLabel">DAFTAR</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="index.php" method="POST">
+                    <div class="modal-body">
+                        <form action="proses_register.php" method="POST">
+                            <div class="modal-body">
+                                <label for="nama">Nama Lengkap</label>
+                                <input required type="text" class="form-control" id="nama"
+                                    placeholder="Masukan nama lengkap" name="nama">
+                            </div>
+                            <div class="modal-body">
+                                <label for="email">Email</label>
+                                <input required type="email" class="form-control" id="email"
+                                    placeholder="Masukkan email" name="email">
+                            </div>
+                            <div class="modal-body">
+                                <label for="password">Password</label>
+                                <input required type="password" class="form-control" id="password"
+                                    placeholder="Masukkan password" name="password">
+                            </div>
+                            <div class="modal-body mb-3">Sudah punya akun? <a class="login" href="#loginModal"
+                                    data-dismiss="modal" data-toggle="modal">Masuk</a></div>
+                        </form>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                            <button type="button" class="btn btn-primary">Daftar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
