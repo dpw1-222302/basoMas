c861943cc8d8abc85bc82cec795d378b98500f6a
destucr <destucr@gmail.com>
2023-07-25 13:13:44 +0700

    fix main

--- connect.php
+++ connect.php
@@ -2,11 +2,7 @@
 $host = 'localhost'; // Nama host database
 $username = 'root'; // Username database
 $password = ''; // Password database
-<<<<<<< HEAD
-$database = 'basoMas'; // Nama database
-=======
 $database = 'basomas'; // Nama database
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
 // Buat koneksi
 $conn = new mysqli($host, $username, $password, $database);
 // Periksa koneksi
@@ -14,9 +10,5 @@ if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
 // Jika koneksi berhasil, dapatkan pesan sukses
-<<<<<<< HEAD
-//echo "Koneksi berhasil";
-=======
 echo "Koneksi berhasil";
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
 ?>
--- index.php
+++ index.php
@@ -6,12 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baso Mas</title>
     <link rel="stylesheet" href="assets/style.css">
-<<<<<<< HEAD
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-=======
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
 
@@ -22,12 +18,8 @@
         <a class="navbar-brand" href="#">
             <img class="logo" src="assets/img/logo-basoMas.png" alt="">
         </a>
-<<<<<<< HEAD
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-=======
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -39,14 +31,7 @@
                     <a class="nav-link text-dark" href="restaurant.html">Restaurant</a>
                 </li>
                 <li class="nav-item">
-<<<<<<< HEAD
-                    <a class="nav-link text-dark" href="#" data-toggle="modal" data-target="#exampleModal">Fan Reviews</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="#" data-toggle="modal" data-target="#exampleModal">Masuk</a>
-=======
                     <a class="nav-link text-dark" href="#">Fan Reviews</a>
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
                 </li>
             </ul>
 
@@ -71,49 +56,12 @@
                 </div>
                 <form>
                     <div class="container row form-group">
-<<<<<<< HEAD
-                        <form action="#" method="POST">
-                            <input type="range" name="rate" class="col form-control-range" max=10 min=0 id="rate">
-                            <label class="col" for=" formControlRange"> <span>0.0 - 10.0</span></label>
-                            <button type="submit" class="btn btn-primary" name="cari" onclick="cekrate()">Cari</button>
-                        </form>
-=======
                         <input type="range" class="col form-control-range" id="formControlRange">
                         <label class="col" for=" formControlRange"> <span>0.0 - 10.0</span></label>
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
                     </div>
                 </form>
             </div>
         </section>
-<<<<<<< HEAD
-
-
-        <section class="review-section m-lg-4">
-
-            <div class="container-fluid card-deck p-2">
-                <?php
-                include_once 'rating.php';
-
-                $ex = $conn->query($query);
-
-                // menggunakan query sql agar menampilkan data produk dan join kedalam tabel user agar mendapatkan siapa pemilik produk
-                // $r = isset($_POST['rate']);
-                // $query = "SELECT * FROM restaurant JOIN review ON restaurant.resto_id = review.resto_id";
-                // $datas = $conn->query($ex);
-                foreach ($ex as $data) :
-                ?>
-                    <div class=" container card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $data['nama_resto'] ?></h5>
-                            <h6 class="card text"><?= $data['harga'] ?></h6>
-                            <p class="card-text"><?= $data['lokasi'] ?></p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
-=======
         <section class="review-section m-lg-4">
            
             <div class="container-fluid card-deck p-2">
@@ -135,7 +83,6 @@
                         <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
                 </div>
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
                 <?php endforeach ?>
             </div>
             <center class="p-5">
@@ -264,91 +211,6 @@
         <!-- Copyright -->
     </footer>
     <!-- Footer -->
-<<<<<<< HEAD
-
-
-    <!-- Add the Bootstrap CSS and JavaScript files as mentioned above -->
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Login</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="proses_login.php" method="POST">
-                    <div class="modal-body">
-                        <form action="proses_login.php" method="POST">
-                            <div class="text-center">
-                                <label for="email">Email</label>
-                                <input required type="email" class="form-control" id="email" placeholder="Masukan email" name="email">
-                            </div>
-                            <div class="text-center">
-                                <label for="password">Password</label>
-                                <input required type="password" class="form-control" id="password" placeholder="Masukkan password" name="password">
-                            </div>
-                            <div class="text-center">
-                                <a href="register.php">Belum punya akun? Daftar di sini</a>
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary btn-block">Masuk</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
-
-    <script>
-        function cekRate() {
-            var rate = $('#rate').val();
-            $.ajax({
-                type: "POST",
-                url: 'rating.php',
-                data: {
-                    rate: rate
-                }
-            });
-        }
-    </script>
-    <script>
-        $(document).ready(function() {
-            $('.rate').on('change', function() {
-
-                var rate = $('#rate').val();
-                $.ajax({
-                    type: "POST",
-                    url: 'rating.php',
-                    data: {
-                        rate: rate
-                    }
-                });
-            });
-
-            $('.rate').on('click', function() {
-                var rate = $('#rate').val();
-                $.ajax({
-                    type: "POST",
-                    url: 'rating.php',
-                    data: {
-                        rate: rate
-                    }
-                });
-            });
-        });
-    </script>
-
-=======
->>>>>>> b79c358d0772730ee00d1d9433fd465eeab52ee3
 </body>
 
 </html>
