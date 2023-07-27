eb570cbf3b936bab5e564330e5d20a3084e2a49f
destucr <destucr@gmail.com>
2023-07-27 16:33:50 +0700

    final demo

--- admin/restaurant/tables.php
+++ admin/restaurant/tables.php
@@ -344,10 +344,10 @@
                         <span aria-hidden="true">x</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Select "Sign Out" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="../../logout.php">Logout</a>
+                    <a class="btn btn-dark" href="../../logout.php">Sign Out</a>
                 </div>
             </div>
         </div>
